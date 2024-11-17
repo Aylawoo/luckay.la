5f275bce0413c16beaa29d4b9d0b3e01fd839651
Aylawoo <luckaylawoo@gmail.com>
2024-11-17 00:47:11 -0600

    Prepare for reverse proxy

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
+from os import getenv
+from dotenv import load_dotenv
 from sanic import Sanic, request
 
+load_dotenv()
+
 app = Sanic("luck-ayla")
-app.extend(config={
-    "templating_path_to_templates": "html/"
-}
-)
+app.config.FORWARDED_SECRET = getenv("SANIC_ID")
+app.extend(config={"templating_path_to_templates": "html/"})
 app.static("static/", "static/")
 
 
@@ -12,3 +14,7 @@ app.static("static/", "static/")
 @app.ext.template("home.html")
 async def home(req: request) -> dict:
     return {}
+
+
+if __name__ == "__main__":
+    app.run("127.0.0.1", 8000, fast=True, access_log=False)
