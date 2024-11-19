705aec6461dbcb8e438741e3e5310d453c8b1efa
Aylawoo <luckaylawoo@gmail.com>
2024-11-19 02:20:05 -0600

    Prep placeholder site for upload

--- src/blueprints/primary.py
+++ src/blueprints/primary.py
@@ -12,6 +12,7 @@ config = json.loads((Path("html/primary.json")).read_text())
 # Jinja
 environment = Environment(loader=FileSystemLoader(Path("html")))
 template_home = environment.get_template("home.jinja")
+template_construction = environment.get_template("construction.jinja")
 
 # Sanic
 host = "localhost:8000" if config["debug"] else "luckay.la"
@@ -21,3 +22,9 @@ home = Blueprint("primary", host=host)
 @home.route("/")
 async def primary_home(req: Request) -> HTTPResponse:
     return html(template_home.render(active="home", **config))
+
+@home.route("/projects", name="projects")
+@home.route("/blog", name="blog")
+@home.route("/about", name="about")
+async def primary_construction(req: Request) -> HTTPResponse:
+    return html(template_construction.render(active="home", **config))
